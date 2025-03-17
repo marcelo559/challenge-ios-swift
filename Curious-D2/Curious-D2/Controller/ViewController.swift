4d5241f009d60eedda9b0197034cb020f542b2c6
Marcelo Campos <marcelo559@gmail.com>
2025-03-16 23:16:44 -0300

    Auto Layout Fix
    I didn't know how to use Corner Radius and width at buttons, and now I fix:
    - Corner radius, corner width and color at buttons;
    - Corner radius at box on the top and Score box;

--- Curious-D2/Curious-D2/Controller/ViewController.swift
+++ Curious-D2/Curious-D2/Controller/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,31 +25,34 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber].answer
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
-        if userAnswer == actualAnswer {
+        print("Item: \(userGotItRight)")
+        
+        if userGotItRight {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
         }
-        
-        if questionNumber + 1 < quiz.count {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-        }
+            
+        quizBrain.nextQuestion()
         
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        
-        updateUI()
     }
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "\(quizBrain.getScore())"
         trueButton.backgroundColor = UIColor.clear
+        trueButton.layer.cornerRadius = 10
+        trueButton.layer.borderWidth = 5
+        trueButton.layer.borderColor = CGColor.init(red: 240, green: 239, blue: 252, alpha: 100)
         falseButton.backgroundColor = UIColor.clear
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        falseButton.layer.cornerRadius = 10
+        falseButton.layer.borderWidth = 5
+        falseButton.layer.borderColor = CGColor.init(red: 240, green: 239, blue: 252, alpha: 100)
     }
 }
 
--- Curious-D2/Curious-D2/Model/QuizBrain.swift
+++ Curious-D2/Curious-D2/Model/QuizBrain.swift
@@ -9,11 +9,70 @@ import Foundation
 
 struct QuizBrain {
     let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Mickey Mouse was Walt Disney's first animated character.", a: "False"),
+        Question(q: "The Evil Queen from Snow White and the Seven Dwarfs has a real name: Grimhilde.", a: "True"),
+        Question(q: "Donald Duck has a twin sister named Della Duck.", a: "True"),
+        Question(q: "Hercules is the only Disney prince who is a god.", a: "True"),
+        Question(q: "Elsa and Anna from Frozen were originally planned to be unrelated characters.", a: "True"),
+        Question(q: "Simba means 'king' in Swahili.", a: "False"),
+        Question(q: "Mulan is the only Disney Princess who is based on a historical figure.", a: "True"),
+        Question(q: "Pinocchio’s nose grows shorter when he tells a lie.", a: "False"),
+        Question(q: "Dumbo is the only main Disney character who never speaks in his film.", a: "True"),
+        Question(q: "In The Little Mermaid, Ariel’s father King Triton is the son of Poseidon.", a: "False"),
+        Question(q: "Walt Disney World in Florida is larger than the city of San Francisco.", a: "True"),
+        Question(q: "Disneyland was built in less than a year.", a: "False"),
+        Question(q: "The 'It's a Small World' ride was originally created for the 1964 New York World's Fair.", a: "True"),
+        Question(q: "There is an underground tunnel system beneath Magic Kingdom.", a: "True"),
+        Question(q: "Cinderella Castle at Walt Disney World is made of real stone.", a: "False"),
+        Question(q: "Tokyo Disneyland was the first Disney park built outside the U.S.", a: "True"),
+        
+        Question(q: "Walt Disney personally opened Walt Disney World.", a: "False"),
+        Question(q: "The Haunted Mansion ride exists in multiple Disney Parks around the world.", a: "False"),
+        Question(q: "The Hollywood Tower Hotel (Tower of Terror) is based on a real hotel.", a: "True"),
+        Question(q: "Expedition Everest at Disney’s Animal Kingdom is the tallest attraction at Walt Disney World.", a: "False"),
+        Question(q: "There is a secret exclusive club at Disneyland called Club 21.", a: "False"),
+        Question(q: "The Cinderella Castle Suite in Walt Disney World is available for public booking.", a: "False"),
+        Question(q: "The voice of Mickey Mouse and the voice of Minnie Mouse got married in real life.", a: "True"),
+        Question(q: "All Disney Princesses must be born into royalty to be considered official princesses.", a: "False"),
+        Question(q: "The Hitchhiking Ghosts in The Haunted Mansion have names: Ezra, Gus, and Phineas.", a: "True"),
+        Question(q: "Space Mountain was the first roller coaster built at Walt Disney World.", a: "True"),
+        Question(q: "Pirates of the Caribbean was one of Disneyland's opening-day attractions.", a: "False"),
+        Question(q: "In Disneyland, the gas lamps on Main Street U.S.A. are real antique lamps.", a: "True"),
+        Question(q: "No one is allowed to scatter ashes in Disney Parks.", a: "False"),
+        Question(q: "There is a time capsule buried at Sleeping Beauty Castle in Disneyland.", a: "True"),
     ]
     
     var questionNumber = 0
+    var score = 0
+    
+    mutating func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            score += 1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getScore() -> Int {
+        return score
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float(questionNumber) / Float(quiz.count)
+        return progress
+    }
+    
+    mutating func nextQuestion() {
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+            score = 0
+        }
+    }
 }
